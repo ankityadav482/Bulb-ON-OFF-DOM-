2bfcdc8ee359487009c417cfeab2b6b039575864
Ankit Yadav <yadavankit811549@gmail.com>
2024-10-10 11:44:11 +0530

    first commit

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
+var istatus = document.querySelector("h3")
 var btn = document.querySelector("button")
-var blb = document.querySelector("#bulb")
-
-var flag =0;
+var flag=0
 btn.addEventListener("click",function(){
     if(flag==0){
-        blb.style.backgroundColor="yellow"
-        btn.innerHTML="Off"
+        istatus.innerHTML="Friend"
+        istatus.style.color="green"
+        btn.innerHTML="Remove Friend"
+        btn.style.backgroundColor="rgba(0, 255, 255, 0.468)"
+        btn.style.color="blue"
         flag=1
-
     }else{
-        blb.style.backgroundColor="blue"
-        btn.innerHTML="Click"
+        istatus.innerHTML="Stranger"
+        istatus.style.color="red"
+        btn.innerHTML="Add Friend"
+         btn.style.backgroundColor="darkkhaki"
+        btn.style.color="white"
         flag=0
     }
 })
